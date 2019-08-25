16b0c5a11f62edeb0105afcd8526cfa2f89ff05a
Sebastian Brych <sebastian+git@nwscx.com>
2019-08-25 14:39:53 +0200

    Add Faraday and manage env vars

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+ENVied.require(*ENV["ENVIED_GROUPS"] || Rails.groups)
 
 module DealViewer
   class Application < Rails::Application

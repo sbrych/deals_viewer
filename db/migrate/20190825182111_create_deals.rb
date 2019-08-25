faa8bc7e0ea55f6ee7d11596b9782b3bb3259401
Sebastian Brych <sebastian+git@nwscx.com>
2019-08-25 20:33:17 +0200

    introduce deal model

---
+++ db/migrate/20190825182111_create_deals.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CreateDeals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :deals do |t|
+      t.integer :external_id, null: false
+      t.string :name, null: false
+      t.float :value, null: false
+      t.integer :deal_stage_id, null: false
+      t.integer :deal_stage_percentage, null: false
+
+      t.timestamps
+    end
+
+    add_index :deals, :external_id, unique: true
+  end
+end
